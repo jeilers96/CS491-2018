bbb8db02d97e1521a6652b1e02e42cb8b0c4fb5c
jeilers96 <jeilers96@gmail.com>
2018-02-09 14:51:19 -0600

    add my assets to the project

---
+++ Assets/scripts/character_test_controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_test_controller : MonoBehaviour {
+	
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	bool grounded = false;
+	public Transform groundCheck;
+	Rigidbody2D rigid2D;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 300f;
+	
+	// Use this for initialization
+	void Start () {
+		rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		float move = Input.GetAxis ("Horizontal");
+		
+		rigid2D.velocity = new Vector2(move * maxSpeed, rigid2D.velocity.y);
+		
+		if(move > 0 && !facingRight) {
+			flip();
+		} else if(move < 0 && facingRight){
+			flip ();
+		}
+	}
+	
+	void Update(){
+		if(grounded && Input.GetKeyDown(KeyCode.UpArrow)) {
+			rigid2D.AddForce(new Vector2(0, jumpForce));
+		}
+	}
+	
+	void flip() {
+		
+		facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+	}
+}
