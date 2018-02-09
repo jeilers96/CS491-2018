050ab2d7954bc02fba5c8632a6809a00327ff48e
Braden Lahr <lahrbt5393@uwec.edu>
2018-02-09 15:03:08 -0600

    Added assets for speed-based character

---
+++ Assets/scripts/characterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour {
+	public float maxSpeed = 3f;
+	Rigidbody2D rigid2D;
+	
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 250f;
+	
+	// Use this for initialization
+	void Start () {
+		rigid2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		
+		float move = Input.GetAxis("Horizontal");
+		rigid2D.velocity = new Vector2(move * maxSpeed, rigid2D.velocity.y);
+	}
+	
+	void Update() {
+		if(grounded && Input.GetKeyDown(KeyCode.UpArrow)) {
+			rigid2D.AddForce(new Vector2(0, jumpForce));
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			maxSpeed *= 3f;
+		} else if (Input.GetKeyUp(KeyCode.Space)){
+			maxSpeed = 3f;
+		}
+	}
+}
