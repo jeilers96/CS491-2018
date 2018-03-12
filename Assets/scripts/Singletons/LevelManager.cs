2812564d9f7e72c1416b27ddf8194d5d9457d77d
pientknk <pientknk@uwec.edu>
2018-03-12 14:22:34 -0500

    created levelmanager to handle character swapping/camera movement and updated camera move to keep player 2 in range of player one

---
+++ Assets/scripts/Singletons/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	//static instance for other scripts to reference
+	public static LevelManager instance;
+	public Transform playerOne;
+	public Transform playerTwo;
+	public List<GameObject> playerOneCharacters;
+	public List<GameObject> playerTwoCharacters;
+
+	private float pauseGameSpeed = 0.0f;
+	private float regularGameSpeed = 1.0f;
+
+	void Awake(){
+		instance = this;
+	}
+
+	void PlayerOneSwap(GameObject newCharacter){
+		playerOne = newCharacter.transform;
+	}
+
+	void PlayerTwoSwap(GameObject newCharacter){
+		playerTwo = newCharacter.transform;
+	}
+}
--- Assets/scripts/camera/CameraFollow.cs
+++ Assets/scripts/camera/CameraFollow.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-	public Transform target;
 	public float smoothSpeed = 0.125f;
 	public Vector3 offset;
 
+	private LevelManager levelManager;
+
+	void Start(){
+		levelManager = LevelManager.instance;
+	}
+
 	void FixedUpdate(){
-		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+		if (levelManager.playerOne != null) {
+			Vector3 desiredPosition = levelManager.playerOne.position + offset;
+			Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed);
+			transform.position = smoothedPosition;
+		}
+
+		if (levelManager.playerTwo != null) {
+			Vector3 player2RequiredPos = Camera.main.WorldToViewportPoint (levelManager.playerTwo.position);
+			player2RequiredPos.x = Mathf.Clamp01(player2RequiredPos.x);
+			player2RequiredPos.y = Mathf.Clamp01(player2RequiredPos.y);
+			levelManager.playerTwo.position = Camera.main.ViewportToWorldPoint(player2RequiredPos);
+		}
 	}
 }
