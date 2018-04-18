aab5ce1bcc3fda9aec44311018cc2ccde6457e2e
Braden Lahr <lahrbt5393@uwec.edu>
2018-04-18 14:20:14 -0500

    Added TileMapManager object to work with spike tiles

---
+++ Assets/scripts/Singletons/TilemapManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour {
+	public GameObject spikeObject;
+	
+	// Use this for initialization
+	void Start () {
+		Tilemap map = GetComponent<Tilemap>();
+		
+		// BoundsInt mapBounds = map.cellBounds;
+		// TileBase[] allTiles = map.GetTilesBlock(mapBounds);
+		
+		
+		// foreach(TileBase tile in allTiles) {
+			// if(tile != null && tile.name == "spike") {
+				// Debug.Log(tile.transform.position.x);
+			// }
+		// }
+		
+		for(int x = map.cellBounds.xMin; x < map.cellBounds.xMax; x++) {
+			for(int y = map.cellBounds.yMin; y < map.cellBounds.yMax; y++) {
+				Vector3Int tileLocation = new Vector3Int(x, y, (int) map.transform.position.y);
+				TileBase mapTile = map.GetTile(tileLocation);
+				if(mapTile != null && mapTile.name == "spike") {
+					Instantiate(spikeObject, map.CellToWorld(tileLocation), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
